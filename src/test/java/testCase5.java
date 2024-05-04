69a5796b0821a7d34d4ca1761c2d474961ad35cd
JarkynK <76789380+JarkynK@users.noreply.github.com>
2024-05-04 16:43:44 +0500

    test case 5 complete

--- src/test/java/testCase5.java
+++ src/test/java/testCase5.java
@@ -1,5 +1,8 @@
 public class testCase5 {
     public static void main(String[] args) {
         System.out.println("this s our test case");
+        System.out.println("this s our test case2");
+        System.out.println("this s our test case3");
+        System.out.println("this s our test case4");
     }
 }
