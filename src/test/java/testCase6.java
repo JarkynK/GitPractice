d2cb0685bc42a2863071f4f863f15ea339e156b4
JarkynK <76789380+JarkynK@users.noreply.github.com>
2024-05-04 18:07:44 +0500

    this is push

--- src/test/java/testCase6.java
+++ src/test/java/testCase6.java
@@ -3,5 +3,7 @@ public class testCase6 {
         System.out.println("test case of 6 ");
         System.out.println("test case of 7 ");
         System.out.println("test case of 8 ");
+        System.out.println("test case of 9 ");
+        System.out.println("test case of 10 ");
     }
 }
