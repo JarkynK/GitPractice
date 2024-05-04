4253cea0b172e19b1a2b5dffbf4655a7fec31885
JarkynK <76789380+JarkynK@users.noreply.github.com>
2024-05-04 17:45:54 +0500

    pushing from termnal

--- src/test/java/testCase6.java
+++ src/test/java/testCase6.java
@@ -1,5 +1,7 @@
 public class testCase6 {
     public static void main(String[] args) {
         System.out.println("test case of 6 ");
+        System.out.println("test case of 7 ");
+        System.out.println("test case of 8 ");
     }
 }
