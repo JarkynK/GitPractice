d3524718c4ba4124da56dc50fc1278b364c671fa
JarkynK <76789380+JarkynK@users.noreply.github.com>
2024-05-04 18:15:05 +0500

    new changes

--- src/test/java/testCase6.java
+++ src/test/java/testCase6.java
@@ -1,5 +1,9 @@
 public class testCase6 {
     public static void main(String[] args) {
         System.out.println("test case of 6 ");
+        System.out.println("test case of 7 ");
+        System.out.println("test case of 8 ");
+        System.out.println("test case of 9 ");
+        System.out.println("test case of 10 ");
     }
 }
