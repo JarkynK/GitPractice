b681c626b88cad652755f63b77a9916a61c02bca
JarkynK <76789380+JarkynK@users.noreply.github.com>
2024-05-04 20:57:44 +0500

    pushing from other btranch

--- src/test/java/GitHubClass.java
+++ src/test/java/GitHubClass.java
@@ -1,5 +1,7 @@
 public class GitHubClass {
     public static void main(String[] args) {
         System.out.println("first");
+        System.out.println("second");
+        System.out.println("third");
     }
 }
