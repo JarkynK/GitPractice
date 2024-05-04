6a27b2089555d0b23c6ce9419bd6246bc8ab0fa2
JarkynK <76789380+JarkynK@users.noreply.github.com>
2024-05-04 16:30:26 +0500

    Update GitHubClass.java
    change from master branch

--- src/test/java/GitHubClass.java
+++ src/test/java/GitHubClass.java
@@ -1,5 +1,9 @@
 public class GitHubClass {
     public static void main(String[] args) {
         System.out.println("first");
+        System.out.println("2 second ");
+
+        System.out.println("3 third");
+
     }
 }
